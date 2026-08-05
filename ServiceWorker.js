cd8dd340d513fcbae01f1ad2c36dbf9ae1d50576
juyounginpark <0620ffff@naver.com>
2026-08-05 17:47:52 +0900

    hi

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-hamdefense-0.1.0-rotate-left-2";
+const cacheName = "DefaultCompany-hamdefense-0.1.0-rotate-left-3";
 const contentToCache = [
     "Build/HamDefense.loader.js",
     "Build/HamDefense.framework.js",
