9605e70e6e3c82c532d38f37d670228aca36d3de
juyounginpark <0620ffff@naver.com>
2026-08-29 04:34:14 +0900

    Fix webgl template loading paths for compression

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-hamdefense-0.1.0";
+const cacheName = "DefaultCompany-hamdefense-0.1.1";
 const contentToCache = [
     "Build/HamDefense.loader.js",
     "Build/HamDefense.framework.js.unityweb",
