bd351437dde40db03202b557f476bd5210e509e7
juyounginpark <0620ffff@naver.com>
2026-08-30 17:32:32 +0900

    ver Alpha 1.0.1

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "DefaultCompany-hamdefense-alpha-1.0.0";
+const cacheName = "DefaultCompany-hamdefense-0.1.0";
 const contentToCache = [
     "Build/HamDefense.loader.js",
     "Build/HamDefense.framework.js.unityweb",
@@ -10,8 +10,7 @@ const contentToCache = [
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-    self.skipWaiting();
-
+    
     e.waitUntil((async function () {
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: app shell and content');
@@ -19,25 +18,16 @@ self.addEventListener('install', function (e) {
     })());
 });
 
-self.addEventListener('activate', function (e) {
-    e.waitUntil((async function () {
-      const keys = await caches.keys();
-      await Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
-      await self.clients.claim();
-    })());
-});
-
 self.addEventListener('fetch', function (e) {
-    if (e.request.method !== 'GET') { return; }
-
     e.respondWith((async function () {
-      try {
-        const response = await fetch(e.request);
-        const cache = await caches.open(cacheName);
-        cache.put(e.request, response.clone());
-        return response;
-      } catch (error) {
-        return caches.match(e.request);
-      }
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
     })());
 });
